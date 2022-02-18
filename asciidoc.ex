5b54f85f8c5a4ee52ff730d1a119b64a86869ed4
Andrew Glassman <andrew.glassman@gmail.com>
2022-02-18 10:00:49 -0600

    Updated copy target.

--- asciidoc.ex
+++ asciidoc.ex
@@ -1,6 +1,6 @@
 defmodule Mix.Tasks.Asciidoc.Clean do
   @moduledoc """
-  Cleans ./asciidoc/generated and ./priv/static/internal-docs/
+  Cleans ./asciidoc/generated and ./priv/static/asciidoc/
   """
   @shortdoc "Cleans generated asciidoc."
 
@@ -10,7 +10,7 @@ defmodule Mix.Tasks.Asciidoc.Clean do
   def run(_) do
     Mix.shell().info("Cleaning Asciidoc - start")
     Mix.shell().cmd("rm -rf ./asciidoc/generated")
-    Mix.shell().cmd("rm -rf ./priv/static/internal-docs")
+    Mix.shell().cmd("rm -rf ./priv/static/asciidoc")
     Mix.shell().info("Cleaning Asciidoc - complete")
   end
 end
@@ -27,24 +27,23 @@ defmodule Mix.Tasks.Asciidoc.Generate do
   def run(_) do
     Mix.shell().info("Generating Asciidoc - start")\
     Mix.shell().cmd("docker run -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -D /documents/asciidoc/generated asciidoc/src/index.adoc")
-    Mix.shell().cmd("docker run -v $PWD:/documents/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram -D /documents/asciidoc/generated asciidoc/src/dev_process.adoc")
     Mix.shell().info("Generating Asciidoc - complete")
   end
 end
 
 defmodule Mix.Tasks.Asciidoc.Copy do
   @moduledoc """
-  Copies generated asciidoc to priv/static/internal-docs
+  Copies generated asciidoc to priv/static/asciidoc
   """
-  @shortdoc "Copies generated asciidoc to priv/static/internal-docs."
+  @shortdoc "Copies generated asciidoc to priv/static/asciidoc."
 
   use Mix.Task
 
   @impl Mix.Task
   def run(_) do
     Mix.shell().info("Copying Asciidoc - start")
-    Mix.shell().cmd("mkdir -p priv/static/admin")
-    Mix.shell().cmd("rsync -a asciidoc/generated/ priv/static/admin/internal-docs/ --exclude=.asciidoctor -v")
+    Mix.shell().cmd("mkdir -p priv/static/asciidoc")
+    Mix.shell().cmd("rsync -a asciidoc/generated/ priv/static/asciidoc/ --exclude=.asciidoctor -v")
     Mix.shell().info("Copying Asciidoc - complete")
   end
 end
